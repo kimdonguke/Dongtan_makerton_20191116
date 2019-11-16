01a2be1a9fd03d6ac22443d2cabf9b1a46410385
kimdonguke <rlaehddnr100@gmail.com>
2019-11-16 15:35:29 +0900

    gimoring
    asdfasdf

--- app/src/main/java/com/example/dongtan_makerton_20191116/MainActivity.java
+++ app/src/main/java/com/example/dongtan_makerton_20191116/MainActivity.java
@@ -4,11 +4,10 @@ import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import app.akexorcist.bluetotohspp.library.BluetoothSPP;
